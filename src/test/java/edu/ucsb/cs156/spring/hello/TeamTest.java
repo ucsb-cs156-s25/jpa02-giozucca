7e9c4bc01e1110dfa6205addf190229e10ef6bad
giozucca <80614385+giozucca@users.noreply.github.com>
2025-04-09 00:55:54 -0700

    get to 100% mutation and line coverage

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -19,6 +19,120 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void hashCode_returns_same_for_same_objects() {
+        Team t1 = new Team();
+        t1.setName("foo");
+        t1.addMember("bar");
+        Team t2 = new Team();
+        t2.setName("foo");
+        t2.addMember("bar");
+        assertEquals(t1.hashCode(), t2.hashCode());
+     
+    }
+
+    @Test
+    public void empty_hash() {
+        // instantiate t as a Team object
+        Team t = new Team();
+        int result = t.hashCode();
+        int expectedResult = 1;
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void same_object_Test() {
+        Team t1 = new Team();
+        t1.setName("foo");
+
+        assertEquals(t1.equals(t1), true);
+    }
+
+    // @Test
+    // public void same_object_Test() {
+    //     Team t1 = new Team();
+    //     t1.setName("foo");
+
+    //     Team t2 = t1;
+
+    //     // assertEquals(t1.getName(), t2.getName());
+
+    //     assertEquals(t1.equals(t2), true);
+    // }
+
+
+    @Test
+    public void not_even_instance() {
+        Team t1 = new Team();
+        int t2 = 6;
+
+        assertEquals(t1.equals(t2), false);
+    }
+
+    @Test
+    public void same_name_same_team() {
+        Team t1 = new Team();
+        t1.setName("foo");
+        t1.addMember("gio");
+        t1.addMember("john");
+
+        Team t2 = new Team();
+        t2.setName("foo");
+        t2.addMember("gio");
+        t2.addMember("john");
+
+        assertEquals(t1.equals(t2), true);
+    }
+
+    @Test
+    public void same_name_diff_team() {
+        Team t1 = new Team();
+        t1.setName("foo");
+        t1.addMember("gio");
+        t1.addMember("john");
+
+        Team t2 = new Team();
+        t2.setName("foo");
+        t2.addMember("gio");
+        t2.addMember("paul");
+
+        assertEquals(t1.equals(t2), false);
+    }
+
+    @Test
+    public void diff_name_same_team() {
+        Team t1 = new Team();
+        t1.setName("foo");
+        t1.addMember("gio");
+        t1.addMember("john");
+
+        Team t2 = new Team();
+        t2.setName("bar");
+        t2.addMember("gio");
+        t2.addMember("john");
+
+        assertEquals(t1.equals(t2), false);
+    }
+
+    @Test
+    public void diff_name_diff_team() {
+        Team t1 = new Team();
+        t1.setName("foo");
+        t1.addMember("gio");
+        t1.addMember("john");
+
+        Team t2 = new Team();
+        t2.setName("bar");
+        t2.addMember("george");
+        t2.addMember("john");
+
+        assertEquals(t1.equals(t2), false);
+    }
    
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
